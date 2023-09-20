f46cc54b54c154dbd49ff03c451cce9a04a0e8a1
admin <admin@gmail.com>
2023-09-21 01:33:33 +0800

    First Init

---
+++ advaspire-front-end/deploy.sh
@@ -0,0 +1,10 @@
+echo "Switching to branch master"
+git checkout master
+
+echo "Building app ... "
+npm run build
+
+echo "Deploying files to server"
+scp -r build/* admin@143.42.72.14:/var/www/143.42.72.14/
+
+echo "Done!!"
